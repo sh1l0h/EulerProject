fa8f91dbfe73c98572b09a24b3f30faaa963dc63
Mirian-Shilakadze <mirian.shilakadze@gmail.com>
2022-02-23 23:02:58 +0400

    Make 15th solution faster

--- ex15.py
+++ ex15.py
@@ -2,10 +2,14 @@ dic = {}
 def solution(x,y):
     if x == 1 or y == 1:
         return x*y + 1
+    if (x, y) in dic:
+        return dic[(x,y)]
     if not (x-1, y) in dic:
         dic[(x-1,y)] = solution(x - 1,y)
     if not (x,y-1) in dic:
         dic[(x,y-1)] = solution(x, y-1)
-    return dic[(x,y-1)] + dic[(x-1,y)]
+    tmp = dic[(x,y-1)] + dic[(x-1,y)]
+    dic[(x,y)] = tmp 
+    return tmp 
 
 print(solution(20,20))
